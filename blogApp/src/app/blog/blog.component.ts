1916f2a922aba934923221cf8a02322072532a13
Rodrigo Fraile Esteban <rofraiesjob@gmail.com>
2024-07-07 19:08:14 +0200

    Creación de array de datos, inicialización de dos post y pintado

--- blogApp/src/app/blog/blog.component.ts
+++ blogApp/src/app/blog/blog.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
-export class BlogComponent {
 
+export class BlogComponent {
+  newPost = {
+    title: '',
+    image: '',
+    text: '',
+    date: ''
+  };
+  
+  postList = [
+    {
+      title: 'Publicación 1',
+      image: 'https://via.placeholder.com/100',
+      text: 'Texto del post.',
+      date: new Date().toISOString().substring(0, 10)
+    },
+    {
+      title: 'Publicación 2',
+      image: 'https://via.placeholder.com/100',
+      text: 'Texto del post.',
+      date: new Date().toISOString().substring(0, 10)
+    }
+  ];
 }
